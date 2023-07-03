005310a5c56390b6c05743c53c64759a23d7f028
Joonel <brazhiyolexandr@gmail.com>
2023-07-03 13:05:34 +0300

    Redirection to non-www

--- src/hook.ts
+++ src/hook.ts
@@ -1,34 +1,38 @@
-import type { Handle } from "@sveltejs/kit";
+import { Redirect, type Handle } from "@sveltejs/kit";
 import type { SitemapParams, RO_Sitemap } from "./types";
 import { generateRobots, generateSitemap } from "./utils";
 
 export const sitemapHook =
-  <S extends RO_Sitemap>(sitemap: S, params: SitemapParams<S> | undefined = {}): Handle =>
-  async ({ event, resolve }) => {
-    if (event.url.pathname === "/sitemap.xml") {
-      // Get dynamic custom definition for app routes
-      const routeDefinitions = params.getRoutes ? await params.getRoutes(event) : {};
-      return new Response(generateSitemap(routeDefinitions, event.url.origin, sitemap), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/xml"
+    <S extends RO_Sitemap>(sitemap: S, params: SitemapParams<S> | undefined = {}): Handle =>
+    async ({ event, resolve }) => {
+        if (event.url.origin.startsWith("www.")) {
+            throw new Redirect(301, event.url.href.replace("www.", ""));
         }
-      });
-    }
 
-    if (event.url.pathname === "/robots.txt") {
-      // Get dynamic robots directives
-      const robots = params.getRobots ? await params.getRobots(event) : true;
-
-      // Build and return the robots.txt
-      return new Response(generateRobots<S>(robots, event.url.origin), {
-        headers: {
-          "content-type": "text/plain",
-          // Cache it for 24 hours
-          "cache-control": `max-age=${60 * 60 * 24}`
+        if (event.url.pathname === "/sitemap.xml") {
+            // Get dynamic custom definition for app routes
+            const routeDefinitions = params.getRoutes ? await params.getRoutes(event) : {};
+            return new Response(generateSitemap(routeDefinitions, event.url.origin, sitemap), {
+                status: 200,
+                headers: {
+                    "Content-Type": "application/xml",
+                },
+            });
         }
-      });
-    }
 
-    return resolve(event);
-  };
+        if (event.url.pathname === "/robots.txt") {
+            // Get dynamic robots directives
+            const robots = params.getRobots ? await params.getRobots(event) : true;
+
+            // Build and return the robots.txt
+            return new Response(generateRobots<S>(robots, event.url.origin), {
+                headers: {
+                    "content-type": "text/plain",
+                    // Cache it for 24 hours
+                    "cache-control": `max-age=${60 * 60 * 24}`,
+                },
+            });
+        }
+
+        return resolve(event);
+    };
