90dccfdfe9e84474ddfc1ae4eafd31b9399adf8d
Olexandr Brazhiy <brazhiyolexandr@gmail.com>
2023-07-28 21:05:10 +0300

    urlset param simplified

--- src/utils.ts
+++ src/utils.ts
@@ -55,16 +55,7 @@ export const generateSitemap = <S extends RO_Sitemap>(
     // Build and return sitemap
     // refs : https://github.com/sveltejs/kit/issues/1142#issuecomment-1032407693 https://github.com/Shopify/hydrogen/blob/1de3864214d04d9214e323d7f0a953c7b9309b7b/templates/demo-store/src/routes/sitemap.xml.server.ts
     return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
-xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
-xmlns:pagemap="http://www.google.com/schemas/sitemap-pagemap/1.0"
-xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${Object.entries(routes)
     .map(([r, { path, changeFreq, image, lastMod }]) => {
         return `  <url>
